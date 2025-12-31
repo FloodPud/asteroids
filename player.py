cd553ae450e93fb2e2a5b1ca44bf483d0840f9cd
FloodPud <collin@floodpud.com>
2025-12-30 19:41:23 -0500

    player drawn on screen

---
+++ player.py
@@ -0,0 +1,30 @@
+from constants import PLAYER_RADIUS, LINE_WIDTH
+from circleshape import CircleShape
+import pygame
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen,"white",self.triangle(),LINE_WIDTH)
+
+    def update(self, dt):
+        # Simple movement logic (for example purposes)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.position.x -= 200 * dt
+        if keys[pygame.K_RIGHT]:
+            self.position.x += 200 * dt
+        if keys[pygame.K_UP]:
+            self.position.y -= 200 * dt
+        if keys[pygame.K_DOWN]:
+            self.position.y += 200 * dt
